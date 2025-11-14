d95c2d8977d1eb6d4f623ba108a93c2f8575d7c9
Nelson242004 <gabrielnelson242004@gmail.com>
2025-11-14 22:52:00 +0100

    Create script.js

---
+++ clientes/pizzeria-la-napolitana/clientes/pizzeria-la-napolitana/script.js
@@ -0,0 +1,6 @@
+// Scripts para Pizzería La Napolitana
+// Ruta: clientes/pizzeria-la-napolitana/script.js
+// De momento no hacemos nada, pero aquí podrías
+// agregar en el futuro tracking, popups, etc.
+
+console.log("BioFast - Pizzería La Napolitana cargada.");
